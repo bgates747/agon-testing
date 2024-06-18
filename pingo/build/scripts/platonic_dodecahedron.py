cbee2d1ac171b4abc8288e93342672baa1fd3af6
bgates747 <brandon.gates@gmail.com>
2024-06-18 17:07:18 -0400

    another edit to dodecahedron generator

--- pingo/build/scripts/platonic_dodecahedron.py
+++ pingo/build/scripts/platonic_dodecahedron.py
@@ -7,7 +7,6 @@ def generate_dodecahedron():
     
     # Vertices of a dodecahedron
     vertices = [
-        # (±1, ±1, ±1)
         [-1, -1, -1],
         [-1, -1,  1],
         [-1,  1, -1],
@@ -16,17 +15,14 @@ def generate_dodecahedron():
         [ 1, -1,  1],
         [ 1,  1, -1],
         [ 1,  1,  1],
-        # (0, ±1/φ, ±φ)
         [ 0, -1/phi, -phi],
         [ 0, -1/phi,  phi],
         [ 0,  1/phi, -phi],
         [ 0,  1/phi,  phi],
-        # (±1/φ, ±φ, 0)
         [-1/phi, -phi,  0],
         [-1/phi,  phi,  0],
         [ 1/phi, -phi,  0],
         [ 1/phi,  phi,  0],
-        # (±φ, 0, ±1/φ)
         [-phi,  0, -1/phi],
         [ phi,  0, -1/phi],
         [-phi,  0,  1/phi],
@@ -57,6 +53,24 @@ def generate_dodecahedron():
     
     return vertices, triangles
 
+# Function to create a unique list of vertices from triangles and generate indices
+def generate_unique_vertices_and_indices(vertices, triangles):
+    unique_vertices = []
+    indices = []
+    vertex_map = {}
+
+    for triangle in triangles:
+        index_triangle = []
+        for vertex_index in triangle:
+            vertex_tuple = tuple(vertices[vertex_index])
+            if vertex_tuple not in vertex_map:
+                vertex_map[vertex_tuple] = len(unique_vertices)
+                unique_vertices.append(vertex_tuple)
+            index_triangle.append(vertex_map[vertex_tuple])
+        indices.append(index_triangle)
+    
+    return unique_vertices, indices
+
 # Function to format and print vertices and faces in BBC BASIC code format
 def print_bbc_basic(vertices, faces, precision=8):
     vertex_format = f"{{:.{precision}f}}, {{:.{precision}f}}, {{:.{precision}f}}"
@@ -65,24 +79,28 @@ def print_bbc_basic(vertices, faces, precision=8):
     index_lines = vertex_lines + len(vertices) * 10 + 20  # Adding 20 to give space for a comment line
     
     print(f"{vertex_lines} REM -- VERTICES --")
-    print(f"{vertex_lines + 10} teapot_vertices%={len(vertices)}")
-    print(f"{vertex_lines + 20} teapot_indexes%={len(faces) * 3}")
+    print(f"{vertex_lines + 10} model_vertices%={len(vertices)}")
+    print(f"{vertex_lines + 20} model_indexes%={len(faces) * 3}")
     
     for i, vertex in enumerate(vertices):
         formatted_vertex = vertex_format.format(*vertex)
         print(f"{vertex_lines + 30 + 10 * i} DATA {formatted_vertex}")
     
-    print(f"{vertex_lines + 30 + 10 * len(vertices)} REM")
-    print(f"{index_lines} REM -- INDEXES --")
+    index_start_line = vertex_lines + 30 + 10 * len(vertices) + 10
+    print(f"{index_start_line} REM")
+    print(f"{index_start_line + 10} REM -- INDEXES --")
     
-    index_counter = index_lines + 10
+    index_counter = index_start_line + 20
     for face in faces:
         formatted_face = ", ".join(map(str, face))
         print(f"{index_counter} DATA {formatted_face}")
         index_counter += 10
 
 # Generate the vertices and faces
-vertices, faces = generate_dodecahedron()
+original_vertices, triangles = generate_dodecahedron()
+
+# Generate unique vertices and corresponding indices
+unique_vertices, indices = generate_unique_vertices_and_indices(original_vertices, triangles)
 
 # Print the vertices and faces in BBC BASIC code format with the desired precision
-print_bbc_basic(vertices, faces, precision=8)
+print_bbc_basic(unique_vertices, indices, precision=8)
