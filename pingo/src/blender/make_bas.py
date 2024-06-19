50cb62194e85e844d7c413cfd5cb216ba798449e
bgates747 <brandon.gates@gmail.com>
2024-06-19 19:50:30 -0400

    lots of work on color cube, mainly redoing the uv texture so that it is a 3x2 pixel image for compactness

--- pingo/src/blender/make_bas.py
+++ pingo/src/blender/make_bas.py
@@ -1,10 +1,11 @@
 import numpy as np
-import re
+from agonImages import img_to_rgba8, convert_to_agon_palette
+import PIL.Image as pil
 
 # Import the vertices and faces from the vertices.py file
 from vertices import vertices, faces
 
-def modify_template(template_path, output_path, vertices, faces, precision=8):
+def modify_template(template_path, output_path, vertices, faces, texture_size, precision=8):
     vertex_format = f"{{:.{precision}f}}, {{:.{precision}f}}, {{:.{precision}f}}"
 
     # Read the template file
@@ -45,11 +46,21 @@ def modify_template(template_path, output_path, vertices, faces, precision=8):
     with open(output_path, 'w') as file:
         file.writelines(lines)
 
+def make_texture_rgba(uv_texture_png, uv_texture_rgba8):
+    img = pil.open(uv_texture_png)
+    img_size = img.size
+    img = convert_to_agon_palette(img, 64, 'HSV', transparent_color=None)
+    img_to_rgba8(img, uv_texture_rgba8)
+    return img_size
+
 # Define the paths and call the function
 template_path = 'pingo/src/bas/template.bas'
 output_path = 'pingo/src/bas/cube_obj.bas'  # Modify this path as needed
+uv_texture_png = 'pingo/src/blender/CubeObj/colorcube.png'  # Modify this path as needed
+uv_texture_rgba8 = 'pingo/src/blender/' + uv_texture_png.split('/')[-1].replace('.png', 'rgba8')
 
 # Modify the template and save the output
-modify_template(template_path, output_path, vertices, faces, precision=8)
+texture_size = make_texture_rgba(uv_texture_png, uv_texture_rgba8)
+modify_template(template_path, output_path, vertices, faces, texture_size, precision=8)
 
 print(f"Modified BASIC code has been written to {output_path}")
