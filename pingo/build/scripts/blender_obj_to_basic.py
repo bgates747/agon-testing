66a266cb9861eafead4fc7d0f7ffdb2b27e69c5b
bgates747 <brandon.gates@gmail.com>
2024-06-24 17:54:57 -0400

    home cooked blend to obj export for regular cube

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -118,10 +118,10 @@ if __name__ == '__main__':
     # base_filename, mesh_name, blender_filename, uv_texture_png
     do_these_things = [
         ['cube', 'Cube', 'cubeuv32x32.png'],
-        ['earth', 'Sphere', 'earth160x80.png'],
-        ['HeavyTank', 'HeavyTank', 'colorcube.png'],
-        ['wolf', 'Cube', 'thumb_16.png'],
-        ['pyr', 'Cube', '2x2.png'], 
+        # ['earth', 'Sphere', 'earth160x80.png'],
+        # ['HeavyTank', 'HeavyTank', 'colorcube.png'],
+        # ['wolf', 'Cube', 'thumb_16.png'],
+        # ['pyr', 'Cube', '2x2.png'], 
     ]
 
     for thing in do_these_things:
