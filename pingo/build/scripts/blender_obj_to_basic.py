e00b1b3bfefff128b4c880b818fa13ce52a0b969
bgates747 <brandon.gates@gmail.com>
2024-06-24 22:49:39 -0400

    icosphere1, camera one bump closer

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -129,6 +129,7 @@ if __name__ == '__main__':
         # ['wolf', 'Cube', 'thumb_16.png'],
         # ['pyr', 'Cube', '2x2.png'], 
         ['icosphere', 'Icosphere', 'earth160x80.png'],
+        ['icosphere1', 'Icosphere', 'earth160x80.png'],
     ]
 
     for thing in do_these_things:
