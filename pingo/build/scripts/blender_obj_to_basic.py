2ceae2cd6d4530c60538f6456fff13d5f14b4c61
bgates747 <brandon.gates@gmail.com>
2024-06-25 16:42:28 -0400

    cube3.bas

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -119,7 +119,8 @@ if __name__ == '__main__':
     do_these_things = [
         # ['cube', 'Cube', 'cubeuv32x32.png'],
         # ['cube1', 'Cube', 'cubeuv32x32.png'],
-        ['cube2', 'Cube', 'cubeuv32x32.png'],
+        # ['cube2', 'Cube', 'cubeuv32x32.png'],
+        ['cube3', 'Cube', 'cubeuv32x32.png'],
         # ['earth', 'Sphere', 'earth160x80.png'],
         # ['HeavyTank', 'HeavyTank', 'colorcube.png'],
         # ['wolf', 'Cube', 'thumb_16.png'],
