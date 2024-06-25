b2b9de18b33d2bd2a110ac422e002c474843835c
bgates747 <brandon.gates@gmail.com>
2024-06-25 14:45:29 -0400

    cylinder.bas

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     # base_filename, mesh_name, blender_filename, uv_texture_png
     do_these_things = [
         # ['cube', 'Cube', 'cubeuv32x32.png'],
-        ['cube1', 'Cube', 'cubeuv32x32.png'],
+        # ['cube1', 'Cube', 'cubeuv32x32.png'],
         # ['earth', 'Sphere', 'earth160x80.png'],
         # ['HeavyTank', 'HeavyTank', 'colorcube.png'],
         # ['wolf', 'Cube', 'thumb_16.png'],
@@ -127,6 +127,7 @@ if __name__ == '__main__':
         # ['icosphere_py', 'Icosphere', 'earthico160x76.png'],
         # ['icosphere_py1', 'Icosphere', 'earthico160x76.png'],
         # ['icosphere1', 'Icosphere', 'earthico160x76.png'],
+        ['cylinder', 'Cylinder', 'tictactoe32x32.png']
     ]
 
     for thing in do_these_things:
