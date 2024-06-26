fb8cea7588c0bad26271a52f1e5abbefdf0f2c3f
bgates747 <brandon.gates@gmail.com>
2024-06-25 20:20:56 -0400

    cube.bas from the top

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -126,12 +126,7 @@ if __name__ == '__main__':
 
     # base_filename, mesh_name, blender_filename, uv_texture_png
     do_these_things = [
-        ['cube', 'Cube', 'cubeuv32x32.png'],
-        ['cube1', 'Cube', 'cubeuv32x32.png'],
-        ['cube2', 'Cube', 'cubeuv32x32.png'],
-        ['cube3', 'Cube', 'tictactoe32x32.png'],
-        ['cube4', 'Cube', 'cubeuv32x32.png'],
-        ['cube5', 'Cube', 'cubeuv32x32.png'],
+        ['cube', 'Cube', 'colors64rgb.png'],
         # ['earth', 'Sphere', 'earth160x80.png'],
         # ['HeavyTank', 'HeavyTank', 'colorcube.png'],
         # ['wolf', 'Cube', 'thumb_16.png'],
