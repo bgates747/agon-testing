2d5608e4d3ec6eb4ec60a56762afed1c96307b03
bgates747 <brandon.gates@gmail.com>
2024-06-27 20:42:57 -0400

    obj to basic back to normal coordinate conversions

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -97,14 +97,9 @@ def parse_obj_file(filepath):
             if not parts:
                 continue
             if parts[0] == 'v':
-                # vertices.append([round(float(parts[1]), 6), round(float(parts[2]), 6), round(float(parts[3]), 6)])
-                # vertices.append([round(-float(parts[1]), 6), round(float(parts[2]), 6), round(float(parts[3]), 6)])
-                vertices.append([round(float(parts[2]), 6), round(float(parts[1]), 6), round(float(parts[3]), 6)])
+                vertices.append([round(float(parts[1]), 6), round(float(parts[2]), 6), round(float(parts[3]), 6)])
             elif parts[0] == 'vt':
                 texture_coords.append([sanitize_uv(float(parts[1])), sanitize_uv(float(parts[2]))])
-                # texture_coords.append([sanitize_uv(float(parts[2])), sanitize_uv(float(parts[1]))])
-                # texture_coords.append([sanitize_uv(float(parts[2])), sanitize_uv(1.0-float(parts[1]))])
-                # texture_coords.append([sanitize_uv(1.0-float(parts[2])), sanitize_uv(float(parts[1]))])
             elif parts[0] == 'f':
                 face = []
                 tex_indices = []
