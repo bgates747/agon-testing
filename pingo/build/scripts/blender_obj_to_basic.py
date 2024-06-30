1ad9c58b60629bee2d714992e67a27c47719a5b5
bgates747 <brandon.gates@gmail.com>
2024-06-30 00:19:20 -0400

    earthico1 and 2

--- pingo/build/scripts/blender_obj_to_basic.py
+++ pingo/build/scripts/blender_obj_to_basic.py
@@ -137,7 +137,9 @@ if __name__ == '__main__':
         # ['heavytank5', 'Cube', 'blenderaxes.png'],
 
         # ['lara', 'M000', 'Lara.png'],
-        ['earthico', 'earthico', 'earthico160x76.png'],
+        # ['earthico', 'earthico', 'earthico160x76.png'],
+        ['earthico1', 'earthico1', 'earthico160x76.png'],
+        ['earthico2', 'earthico2', 'earthico160x76.png'],
 
         # ['heavytank3inv', 'Cube', 'blenderaxes.png'],
         # ['cube', 'Cube', 'colors64rgb.png'],
