6713e09c3431fd54db0b00dec46b9a1823b6b32f
bgates747 <brandon.gates@gmail.com>
2024-07-03 01:35:23 -0400

    synch commit

--- pingo/src/asm/testfileload.asm
+++ pingo/src/asm/testfileload.asm
@@ -46,6 +46,10 @@ main:
     ld hl,image_buffer
     ld ix,image_size
     ld iy,image_filename
+
+    ; call dumpRegistersHex
+    ; ret 
+
     call vdu_load_img
 
 ; plot the bitmap
@@ -57,6 +61,112 @@ main:
     
     ret
 
+; load an image file to a buffer and make it a bitmap
+; inputs: a = image type ; bc,de image width,height ; hl = bufferId ; ix = file size ; iy = pointer to filename
+vdu_load_img:
+; back up image type and dimension parameters
+    push af
+	push bc
+	push de
+; load the image
+	call vdu_load_buffer_from_file
+; now make it a bitmap
+	pop de ; image height
+	pop bc ; image width
+	pop af ; image type
+	jp vdu_bmp_create ; will return to caller from there
+
+; inputs: hl = bufferId, ix = file size ; iy = pointer to filename
+vdu_load_buffer_from_file:
+; load buffer ids
+    ld (@id0),hl
+    ld (@id1),hl
+; clean up bytes that got stomped on by the ID loads
+    ld a,2
+    ld (@id0+2),a
+    xor a
+    ld (@id1+2),a
+; load filesize from ix
+    ld (@filesize),ix
+    ld bc,(ix) ; for the mos_load call
+    ld ix,(ix+1) ; now ix is filesize / 256 and will be our main loop counter
+
+    call dumpRegistersHex
+    ret 
+
+; load the file from disk into ram
+    push iy
+	pop hl ; pointer to filename
+	ld de,filedata
+	ld a,mos_load
+	RST.LIL 08h
+; clear target buffer
+    ld hl,@clear0
+    ld bc,@clear1-@clear0
+    rst.lil $18
+    jp @clear1
+@clear0: db 23,0,0xA0
+@id0:	dw 0x0000 ; bufferId
+		db 2 ; clear buffer
+@clear1:
+
+; load default chunk size
+    ld bc,256
+    ld (@chunksize),bc
+; point hl at the start of the file data
+    ld hl,filedata
+
+    ; call dumpRegistersHex
+
+@loop:
+    dec ix
+
+    call dumpRegistersHex
+
+    jp m,@lastchunk
+    jp z,@lastchunk
+    push hl ; store pointer to file data
+
+    ; call dumpRegistersHex
+
+    call @loadchunk ; load the next chunk
+    ; ld a,'.'
+    ; rst.lil 10h
+    jp @loop ; loop back to load the next chunk
+
+@lastchunk:
+    ld bc,0 ; make sure bcu is zero
+    ld a,(ix) ; get the remaining bytes
+    and a
+    ret z ; no more to load so we're done
+    ld c,a ; bc is number of final bytes to load
+    ld (@chunksize),bc  
+    push hl ; store pointer to file data
+    ; fall through to loadchunk
+
+@loadchunk:
+    ld hl,@chunk0
+    ld bc,@chunk1-@chunk0
+    rst.lil $18
+    jp @chunk1
+@chunk0:
+; Upload data :: VDU 23, 0 &A0, bufferId; 0, length; <buffer-data>
+		db 23,0,0xA0
+@id1:	dw 0x0000 ; bufferId
+		db 0 ; load buffer
+@chunksize:	dw 0x0000 ; length of data in bytes
+@chunk1:
+    pop hl ; get the file data pointer
+    ld bc,(@chunksize)
+    rst.lil $18
+    add hl,bc ; advance the file data pointer
+    ret
+
+@filesize: dl 0 ; file size in bytes
+
+
+
+; https://github.com/envenomator/Agon/blob/master/ez80asm%20examples%20(annotated)/functions.s
 ; Print a zero-terminated string
 ; HL: Pointer to string
 printString:
@@ -66,7 +176,63 @@ printString:
 	RST.LIL 18h
 	POP		BC
 	RET
+; print a VDU sequence
+; HL: Pointer to VDU sequence - <1 byte length> <data>
+sendVDUsequence:
+	PUSH	BC
+	LD		BC, 0
+	LD		C, (HL)
+	RST.LIL	18h
+	POP		BC
+	RET
+; Print Newline sequence to VDP
+printNewLine:
+	LD	A, '\r'
+	RST.LIL 10h
+	LD	A, '\n'
+	RST.LIL 10h
+	RET
+; Print a 24-bit HEX number
+; HLU: Number to print
+printHex24:
+	PUSH	HL
+	LD		HL, 2
+	ADD		HL, SP
+	LD		A, (HL)
+	POP		HL
+	CALL	printHex8
+; Print a 16-bit HEX number
+; HL: Number to print
+printHex16:
+	LD		A,H
+	CALL	printHex8
+	LD		A,L
+; Print an 8-bit HEX number
+; A: Number to print
+printHex8:
+	LD		C,A
+	RRA 
+	RRA 
+	RRA 
+	RRA 
+	CALL	@F
+	LD		A,C
+@@:
+	AND		0Fh
+	ADD		A,90h
+	DAA
+	ADC		A,40h
+	DAA
+	RST.LIL	10h
+	RET
 
+; Print a 0x HEX prefix
+DisplayHexPrefix:
+	LD	A, '0'
+	RST.LIL 10h
+	LD	A, 'x'
+	RST.LIL 10h
+	RET
 ; Prints the right justified decimal value in HL without leading zeroes
 ; HL : Value to print
 printDec:
@@ -130,6 +296,97 @@ DivideMe:
 	RET
 
 
+; print registers to screen in hexidecimal format
+; inputs: none
+; outputs: values of every register printed to screen
+;    values of each register in global scratch memory
+; destroys: nothing
+dumpRegistersHex:
+; store everything in scratch
+    ld (uhl),hl
+    ld (ubc),bc
+    ld (ude),de
+    ld (uix),ix
+    ld (uiy),iy
+    push af ; fml
+    pop hl  ; thanks, zilog
+    ld (uaf),hl
+    push af ; dammit
+
+; home the cursor
+    call vdu_home_cursor
+
+; print each register
+    ld hl,str_afu
+    call printString
+    ld hl,(uaf)
+    call printHex24
+    call printNewLine
+
+    ld hl,str_hlu
+    call printString
+    ld hl,(uhl)
+    call printHex24
+    call printNewLine
+
+    ld hl,str_bcu
+    call printString
+    ld hl,(ubc)
+    call printHex24
+    call printNewLine
+
+    ld hl,str_deu
+    call printString
+    ld hl,(ude)
+    call printHex24
+    call printNewLine
+
+    ld hl,str_ixu
+    call printString
+    ld hl,(uix)
+    call printHex24
+    call printNewLine
+
+    ld hl,str_iyu
+    call printString
+    ld hl,(uiy)
+    call printHex24
+    call printNewLine
+
+    call printNewLine
+; restore everything
+    ld hl, (uhl)
+    ld bc, (ubc)
+    ld de, (ude)
+    ld ix, (uix)
+    ld iy, (uiy)
+    pop af
+; all done
+    ret
+
+; global scratch memory for registers
+uaf: dl 0
+uhl: dl 0
+ubc: dl 0
+ude: dl 0
+uix: dl 0
+uiy: dl 0
+usp: dl 0
+upc: dl 0
+
+str_afu: db "af=",0
+str_hlu: db "hl=",0
+str_bcu: db "bc=",0
+str_deu: db "de=",0
+str_ixu: db "ix=",0
+str_iyu: db "iy=",0
+
+; VDU 30: Home cursor
+vdu_home_cursor:
+    ld a,30
+	rst.lil $10  
+	ret
+
 vdu_set_screen_mode:
 	ld (@arg),a        
 	ld hl,@cmd         
@@ -140,14 +397,11 @@ vdu_set_screen_mode:
 @arg: db 0  ; screen mode parameter
 @end:
 
-
 ; VDU 9: Move cursor forward one character
 vdu_cursor_forward:
     ld a,9
 	rst.lil $10  
 	ret
-
-
 
 ; VDU 23, 27, &20, bufferId; : Select bitmap (using a buffer ID)
 ; inputs: hl=bufferId
@@ -160,7 +414,6 @@ vdu_buff_select:
 @cmd: db 23,27,0x20
 @bufferId: dw 0x0000
 @end: db 0x00 ; padding
-
 
 ; VDU 23, 27, &21, w; h; format: Create bitmap from selected buffer
 ; inputs: a=format; bc=width; de=height
@@ -206,95 +459,5 @@ vdu_plot_bmp:
 @x0: 	dw 0x0000
 @y0: 	dw 0x0000
 @end:   db 0x00 ; padding
-
-
-; load an image file to a buffer and make it a bitmap
-; inputs: a = image type ; bc,de image width,height ; hl = bufferId ; ix = file size ; iy = pointer to filename
-vdu_load_img:
-; back up image type and dimension parameters
-    push af
-	push bc
-	push de
-; load the image
-	call vdu_load_buffer_from_file
-; now make it a bitmap
-	pop de ; image height
-	pop bc ; image width
-	pop af ; image type
-	jp vdu_bmp_create ; will return to caller from there
-
-; inputs: hl = bufferId, ix = file size ; iy = pointer to filename
-vdu_load_buffer_from_file:
-; load buffer ids
-    ld (@id0),hl
-    ld (@id1),hl
-; clean up bytes that got stomped on by the ID loads
-    ld a,2
-    ld (@id0+2),a
-    xor a
-    ld (@id1+2),a
-; load filesize from ix
-    ld (@filesize),ix
-    ld bc,(ix) ; for the mos_load call
-    ld ix,(ix+1) ; now ix is filesize / 256 and will be our main loop counter
-; load the file from disk into ram
-    push iy
-	pop hl ; pointer to filename
-	ld de,filedata
-	ld a,mos_load
-	RST.LIL 08h
-; clear target buffer
-    ld hl,@clear0
-    ld bc,@clear1-@clear0
-    rst.lil $18
-    jp @clear1
-@clear0: db 23,0,0xA0
-@id0:	dw 0x0000 ; bufferId
-		db 2 ; clear buffer
-@clear1:
-
-; load default chunk size
-    ld bc,256
-    ld (@chunksize),bc
-; point hl at the start of the file data
-    ld hl,filedata
-
-@loop:
-    dec ix
-    jp m,@lastchunk
-    jp z,@lastchunk
-    push hl ; store pointer to file data
-    call @loadchunk ; load the next chunk
-    jp @loop ; loop back to load the next chunk
-
-@lastchunk:
-    ld bc,0 ; make sure bcu is zero
-    ld a,(ix) ; get the remaining bytes
-    and a
-    ret z ; no more to load so we're done
-    ld c,a ; bc is number of final bytes to load
-    ld (@chunksize),bc  
-    push hl ; store pointer to file data
-    ; fall through to loadchunk
-
-@loadchunk:
-    ld hl,@chunk0
-    ld bc,@chunk1-@chunk0
-    rst.lil $18
-    jp @chunk1
-@chunk0:
-; Upload data :: VDU 23, 0 &A0, bufferId; 0, length; <buffer-data>
-		db 23,0,0xA0
-@id1:	dw 0x0000 ; bufferId
-		db 0 ; load buffer
-@chunksize:	dw 0x0000 ; length of data in bytes
-@chunk1:
-    pop hl ; get the file data pointer
-    ld bc,(@chunksize)
-    rst.lil $18
-    add hl,bc ; advance the file data pointer
-    ret
-
-@filesize: dl 0 ; file size in bytes
 
 filedata: ; no need to allocate space here if this is the final address label
